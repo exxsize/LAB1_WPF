1bba20ed7d4e2acc312678fbf5d898a5fb6206a1
tomil <tomil@DESKTOP-HRPSOQT>
2022-05-24 15:45:00 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLR1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent ();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(button1, p.X - button1.ActualWidth / 2);
+            Canvas.SetTop(button1, p.Y - button1.ActualHeight / 2);
+            button2.Content = "";
+            button2.MouseMove += button2_MouseMove;
+            button2.Click += button2_Click;
+            button2.Click -= button2_Click2;
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Random r = new Random();
+            if (Keyboard.IsKeyDown(Key.LeftCtrl)
+                || Keyboard.IsKeyDown(Key.RightCtrl)
+                || Keyboard.IsKeyDown(Key.Space))
+                return;
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(button2, r.NextDouble() *
+                ((Content as Canvas).ActualWidth-5));
+            Canvas.SetTop(button2, r.NextDouble() *
+                ((Content as Canvas).ActualHeight - 5));
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            button2.MouseMove -= button2_MouseMove;
+            button2.Click -= button2_Click2;
+        }
+        private void button2_Click2(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Normal ?
+                WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var c = Content as Canvas;
+            for (int i = 0; i < 2; i++)
+            {
+                var b = FindName("button" + (i+1)) as Button;
+                if (Canvas.GetLeft(b)>c.ActualWidth ||
+                    Canvas.GetTop(b) > c.ActualHeight)
+                {
+                    Canvas.SetLeft(b, 10 + i * (b.ActualWidth + 10));
+                    Canvas.SetTop(b, 10);
+
+                }
+            }
+        }
+    }
+}
